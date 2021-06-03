13aefa37a7ab610e8da4d861e5409ab0eae036d3
YTBeater <YTDevelopement@hotmail.com>
2021-06-03 10:37:41 +0200

    - Fichier requirements.txt pour les librairie - main.py pour les appelles - bibliotheque.py pour les fonctions - .travis.yml test à modifier

---
+++ bibliotheque.py
@@ -0,0 +1,15 @@
+#import
+import math
+
+#Fonctions
+def addition(a, b):
+    return a+b
+
+def multiplication(a, b):
+    return a*b
+
+def soustraction(a, b):
+    return a-b
+
+def cosinus(a):
+    return math.cos(a)
---
+++ main.py
@@ -0,0 +1,11 @@
+import bibliotheque
+
+
+#Main
+a = 1
+b = 2    
+
+print(bibliotheque.addition(a,b))
+print(bibliotheque.multiplication(a,b))
+print(bibliotheque.multiplication(a,b))
+print(bibliotheque.cosinus(a))
